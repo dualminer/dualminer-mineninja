fe3c722dad12e74d6e0f05641d99b73c8760722b
dualminer <dualminer@broadeng.net>
2014-03-11 16:42:03 +0800

    add BBB's fs support for USB DualMiner

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 dualminer-mineninja
 ===================
 
-web monitor  and fs support for Pi mining use DualMiner USB  http://www.dualminer.com/  
+web monitor  and fs support for Pi or BBB  mining use DualMiner USB  http://www.dualminer.com/  
 
 
-web monitor and fs support for Pi mining use DualMiner USB 
+web monitor and fs support for Pi or BBB  mining use DualMiner USB 
 
-please reference DualMiner USB raspi user guide  from http://www.dualminer.com/
+please reference DualMiner USB raspi user guide and Beagleboard black user guide from http://www.dualminer.com/
 
---
+++ conf_BBB/dualminer.sql
@@ -0,0 +1,145 @@
+-- MySQL dump 10.13  Distrib 5.1.40, for angstrom-linux-gnueabi (arm)
+--
+-- Host: localhost    Database: anubis
+-- ------------------------------------------------------
+-- Server version	5.1.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accgroups`
+--
+
+DROP TABLE IF EXISTS `accgroups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accgroups` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `currency` varchar(3) NOT NULL DEFAULT 'USD',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accgroups`
+--
+
+LOCK TABLES `accgroups` WRITE;
+/*!40000 ALTER TABLE `accgroups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accgroups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `group` mediumint(6) DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `address` varchar(34) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `configuration`
+--
+
+DROP TABLE IF EXISTS `configuration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `configuration` (
+  `updatetime` int(11) NOT NULL,
+  `yellowtemp` int(11) NOT NULL,
+  `yellowrejects` int(11) NOT NULL,
+  `yellowdiscards` int(11) NOT NULL,
+  `yellowstales` int(11) NOT NULL,
+  `yellowgetfails` int(11) NOT NULL,
+  `yellowremfails` int(11) NOT NULL,
+  `maxtemp` int(11) NOT NULL,
+  `maxrejects` int(11) NOT NULL,
+  `maxdiscards` int(11) NOT NULL,
+  `maxstales` int(11) NOT NULL,
+  `maxgetfails` int(11) NOT NULL,
+  `maxremfails` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `yellowfan` int(11) NOT NULL,
+  `maxfan` int(11) NOT NULL,
+  `yellowgessper` int(11) NOT NULL,
+  `maxgessper` int(11) NOT NULL,
+  `yellowavgmhper` int(11) NOT NULL,
+  `maxavgmhper` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `configuration`
+--
+
+LOCK TABLES `configuration` WRITE;
+/*!40000 ALTER TABLE `configuration` DISABLE KEYS */;
+INSERT INTO `configuration` VALUES (30,80,1,30,7,1,1,84,2,40,10,2,2,'change@me.com',85,90,95,90,95,90);
+/*!40000 ALTER TABLE `configuration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hosts`
+--
+
+DROP TABLE IF EXISTS `hosts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hosts` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `port` mediumint(6) NOT NULL DEFAULT '4028',
+  `mhash_desired` decimal(10,2) NOT NULL,
+  `conf_file_path` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hosts`
+--
+
+LOCK TABLES `hosts` WRITE;
+/*!40000 ALTER TABLE `hosts` DISABLE KEYS */;
+INSERT INTO `hosts` VALUES (1,'DualMiner-BTC','127.0.0.1',4028,'1000.00','/home/xuser/broadeng/conf/cgminer.conf.btc'),(3,'DualMiner-LTC','127.0.0.1',4029,'1000.00','/home/root/cgminer.conf.ltc');
+/*!40000 ALTER TABLE `hosts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-11  8:38:14
